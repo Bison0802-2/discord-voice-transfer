ad33f70920c24ce6b7952d3f16c03980093799f7
Bison0802-2 <kosuke.umemura@akariinc.co.jp>
2024-06-02 20:24:08 +0900

    feat: ping.tsとして書き直し

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const builders_1 = require("@discordjs/builders");
 module.exports = {
-	data: new SlashCommandBuilder()
-        // コマンドの名前
-		.setName('ping')
-        // コマンドの説明文
-		.setDescription('Pong!と返信。'),
-	async execute(interaction) {
-        // Pong!と返信
-		await interaction.reply('Pong!');
-	},
+    data: new builders_1.SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Pong!と返信。"),
+    async execute(interaction) {
+        await interaction.reply("Pong!");
+    },
 };
---
+++ commands/ping.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Pong!と返信。"),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply("Pong!");
+  },
+};
